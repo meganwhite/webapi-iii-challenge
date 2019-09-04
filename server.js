7ece7c468842258de52a1b1996d092f9b8cf1f18
Megan White <mfw247@gmail.com>
2019-09-04 14:41:20 -0700

    set up server, began writing functions

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = 'express';
+const express = require('express');
 
 const server = express();
 
@@ -8,8 +8,31 @@ server.get('/', (req, res) => {
 
 //custom middleware
 
-function logger(req, res, next) {
+const userRouter = require('./users/userRouter')
+const postRouter = require('./posts/postRouter')
 
+
+
+function logger(req, res, next) {
+  console.log(req.body);
 };
+
+function validateUserID(req, res, next) {
+  console.log(req.body)
+  if (Object.keys(req.body).length === 0) {
+    res.status(400).json({message: "No user data"})
+  }
+  else {
+    next()
+  }
+}
+
+server.use(express.json());
+server.use('/users', logger, userRouter);
+server.use('posts', logger, postRouter);
+
+server.get('/', (req,res) => {
+  res.status(200).json({api: 'up'});
+})
 
 module.exports = server;
